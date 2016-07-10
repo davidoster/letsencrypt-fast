9a1e3b9870e30f38f8f337cd1800cb0a0cf80cf9
David Oster aka George Pasparakis <paspa@hotmail.com>
2016-07-10 12:08:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ sudo cpanm Test::More Crypt::LE
 sudo mkdir -p /root/software
 cd /root/software
 wget https://github.com/davidoster/letsencrypt-fast/blob/master/le.sh
-sudo chmod 644 ./le.sh
+sudo chmod 744 ./le.sh
