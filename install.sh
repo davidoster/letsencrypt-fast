2ea3ca62340d2e349d25f940b83b035f57cfb130
David Oster aka George Pasparakis <paspa@hotmail.com>
2016-07-17 07:20:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install make gcc libssl-dev liblocal-lib-perl cpanminus
 sudo cpanm Test::More Crypt::LE
-sudo mkdir -p /root/software
+sudo mkdir -p /root/software/keys
 cd /root/software
 wget https://github.com/davidoster/letsencrypt-fast/blob/master/le.sh
 sudo chmod 744 ./le.sh
