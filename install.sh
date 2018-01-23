f6ee800371e547cc3668ac1aa320bf07cb69dd34
David Oster aka George Pasparakis <info@eletter.gr>
2018-01-23 22:52:54 +0200

    Update to raw version
    Update to raw version

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ sudo apt-get install make gcc libssl-dev liblocal-lib-perl cpanminus
 sudo cpanm Test::More Crypt::LE
 sudo mkdir -p /root/software/keys
 cd /root/software
-wget https://github.com/davidoster/letsencrypt-fast/blob/master/le.sh
+wget https://raw.githubusercontent.com/davidoster/letsencrypt-fast/master/le.sh
 sudo chmod 744 ./le.sh
